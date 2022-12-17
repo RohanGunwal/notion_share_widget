7d719ad7cfc62e19b9c55578a5525413bfd72476
Rohan <yadav.gunwal.rohan@gmail.com>
2022-12-17 22:39:57 +0530

    Added images and data of the users

---
+++ src/utils/data.tsx
@@ -0,0 +1,43 @@
+const users = [
+  {
+    id: 1,
+    name: "Wade Cooper",
+    imageSrc: "/assets/Images/Wade.png",
+    email: "wade@oslash.com",
+  },
+  {
+    id: 2,
+    name: "Arlene Mccoy",
+    imageSrc: "/assets/Images/Arlene.png",
+    email: "arlene@oslash.com",
+  },
+  {
+    id: 3,
+    name: "Tim Cook",
+    imageSrc: "/assets/Images/timCook.png",
+    email: "tim@oslash.com",
+  },
+  {
+    id: 4,
+    name: "Rohan",
+    imageSrc: "",
+    email: "rohan@oslash.com",
+  },
+];
+
+const groups = [
+  {
+    id: 1,
+    group_name: "Product",
+    quantity: 20,
+  },
+  {
+    id: 2,
+    group_name: "Engineering",
+    quantity: 30,
+  },
+];
+
+const options = ["Full access", "Can edit", "Can view", "No access"];
+
+export { users, groups, options };
